07881d75359a6ff47773ccd633a457045667e8e5
zhou <540376482yzb@gmail.com>
2019-05-28 11:50:54 -0700

    save for new version

--- handsomeJ.js
+++ handsomeJ.js
@@ -1,85 +1,69 @@
-const puppeteer = require('puppeteer');
-const devices = require('puppeteer/DeviceDescriptors');
-const chalk = require('chalk');
+const puppeteer = require("puppeteer");
+const devices = require("puppeteer/DeviceDescriptors");
+const chalk = require("chalk");
 
 // TODO: Set up username and password
-const USERNAME = '';
-const PASSWORD = '';
+const USERNAME = "";
+const PASSWORD = "";
 
-getData = async () => {
-  try {
+class App {
+  constructor() {
+    this.page = null;
+  }
+  async init() {
     const browser = await puppeteer.launch({
       headless: false
     });
-
-    const page = await browser.newPage();
-
-    await page.emulate(devices['iPad']);
-
-    //await page.goto('https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fwww.alipay.com%2F&fbclid=IwAR2gunoIayic4A9YVdVXVUrlkJYOCPC5MjbufeToiQGfQurLc-7L3hdit9g');
-    //await page.goto('https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fcustweb.alipay.com%2Faccount%2Findex.htm')
-    //await page.goto('https://consumeprod.alipay.com/record/standard.htm')
-    console.log(chalk.blue('========== 登入网页 ======='));
-    await page.goto('https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fconsumeprod.alipay.com%2Frecord%2Fstandard.htm')
-
-    console.log(chalk.blue('========== 等待点击账号输入 ======='));
-    await page.waitFor('#J-loginMethod-tabs > li:nth-child(2)');
-    await page.click('#J-loginMethod-tabs > li:nth-child(2)');
-    console.log(chalk.green('========== 点击成功 ======='));
-
-    let example = await page.$('#J-checkcode-img');
-
-    while (await example.isIntersectingViewport()) {
-      // The element IS visible within the current viewport.
-      console.log(chalk.blue('========== 有验证码 ========'));
-
-      await page.reload();
-      example = await page.$('#J-checkcode-img');
-      await page.waitFor('#J-loginMethod-tabs > li:nth-child(2)');
-      await page.click('#J-loginMethod-tabs > li:nth-child(2)');
-
-      console.log(chalk.green('========== 点击成功 ======='));
-    }
-
-    console.log(chalk.green('========== 无验证码 ========'));
-
-    console.log(chalk.blue('========== 输入账户开始 ========'));
-
-    await page.waitFor('#J-input-user');
-    await page.type('#J-input-user', USERNAME, { delay: 90 });
-
-    console.log(chalk.green('========== 输入账户结束 ========'));
-
-    console.log(chalk.blue('========== 输入密码开始 ========'));
-
-    await page.waitFor('#password_rsainput');
-    await page.type('#password_rsainput', PASSWORD, { delay: 100 });
-
-    console.log(chalk.green('========== 输入密码结束 ========'));
-
-    console.log(chalk.blue('========== 点击登入 ========'));
-
-    await page.waitFor('#J-login-btn');
-    await page.click('#J-login-btn');
-
-    console.log(chalk.green('========== 登入成功 ========'));
-
-    console.log(chalk.blue('========== 等待寻找数据 ========'));
-    await page.waitForNavigation();
-
-    await page.waitFor('#J_home-record-container');
-
-    console.log(chalk.blue('========== 寻找数据成功 ========'));
-
-    const data = await page.evaluate(() => {
-      const tds = Array.from(document.querySelectorAll('table tr td'))
-      return tds.map(td => td.innerHTML);
-    });
-
-    console.log(data);
-  } catch (e) {
-    console.log(chalk.red(e));
+    this.page = await browser.newPage();
+    await this.page.emulate(devices["iPad"]);
   }
-};
 
-getData();
+  async redirect() {
+    try {
+      console.log(chalk.blue("========== 进入跳转 ========"));
+      await this.page.goto("https://my.alipay.com/portal/i.htm");
+      setTimeout(() => {
+        this.start();
+      }, 5000);
+    } catch (err) {
+      console.log(chalk.red("error when redirect"));
+    }
+  }
+
+  async start() {
+    if (!this.page) {
+      await this.init();
+    }
+    try {
+      await this.page.goto("https://consumeprod.alipay.com/record/standard.htm");
+      console.log("========= 等待抓取数据 ==================");
+      // await this.page.waitForNavigation();
+      await this.page.waitFor("#tradeRecordsIndex");
+      console.log(chalk.blue("========== 寻找数据成功 ========"));
+      const data = await this.page.evaluate(() => {
+        const tds = Array.from(document.querySelectorAll("table tr td"));
+        return tds.map(td => td.innerHTML);
+      });
+      if (data) {
+        console.log("I got it");
+        this.redirect();
+      }
+    } catch (error) {
+      if (
+        error.message === "Navigation Timeout Exceeded: 30000ms exceeded" ||
+        error.message.includes(`waiting for selector`)
+      ) {
+        console.log("restarting");
+        this.start();
+      }
+      console.log(chalk.red(error.message));
+    }
+  }
+}
+
+function run() {
+  var newApp = new App();
+  newApp.start();
+}
+
+run();
