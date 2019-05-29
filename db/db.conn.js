aefec32c380e27423bb4ca7ca85960752fc3316e
Jeremy Guan <jeremyguan1@gmail.com>
2019-05-28 17:16:41 -0700

    good

--- db/db.conn.js
+++ db/db.conn.js
@@ -1,61 +1,58 @@
 const redis = require("od-utility-redis");
 
 class CrawlerDBService {
-
   constructor(hand_token) {
     this.hand_token = hand_token;
   }
 
-  async saveLogs(logs=[]) {
-    if(!this.hand_token) {
-      throw new Error('HAND TOKEN IS NOT INITIALIZED IN CRAWLER')
+  async saveLogs(logs = []) {
+    if (!this.hand_token) {
+      throw new Error("HAND TOKEN IS NOT INITIALIZED IN CRAWLER");
     }
 
-    const oldLogs = await this.findLogs()
-    const newLogs = [...oldLogs, ...logs]
-    console.log('========  saving ...... =============')
-    console.log(newLogs)
-    console.log('========   =============')
-    redis.setAsync("DB-LOG",this.hand_token, newLogs, 2592000);
+    const oldLogs = await this.findLogs();
+    const newLogs = [...oldLogs, ...logs];
+    console.log("========  saving ...... =============");
+    console.log(newLogs);
+    console.log("========   =============");
+    redis.setAsync("DB-LOG", this.hand_token, newLogs, 2592000);
   }
-  
+
   async findLogs(hand_token) {
-    if(!hand_token && !this.hand_token){
-      throw new Error('HAND TOKEN IS NOT INITIALIZED IN CRAWLER')
+    if (!hand_token && !this.hand_token) {
+      throw new Error("HAND TOKEN IS NOT INITIALIZED IN CRAWLER");
     }
 
     const logCache = await redis.getAsync("DB-LOG", hand_token || this.hand_token);
-    console.log('========  finding logs ...... =============')
-    console.log(logCache)
-    console.log('========   =============')
+    console.log("========  finding logs ...... =============");
+    console.log(logCache);
+    console.log("========   =============");
     return logCache;
   }
 
-  async compareLogs(logs=[]) {
-    if(!this.hand_token){
-      throw new Error('HAND TOKEN IS NOT INITIALIZED IN CRAWLER')
+  async compareLogs(logs = []) {
+    if (!this.hand_token) {
+      throw new Error("HAND TOKEN IS NOT INITIALIZED IN CRAWLER");
     }
 
-    const newLogs = []
+    const newLogs = [];
     const cachedLogs = await this.findLogs();
-    for(let log of logs) {
-      const newLog = CrawlerDBService._diffLogInCache(log,cachedLogs);
-      if(newLog) newLogs.push(newLog)
-    } 
-    return newLogs;
-    
-  }
-
-  async static _diffLogInCache(log,cachedLogs) {
-    let flag = false
-    for(let cache of cachedLogs) {
-        if(log.tradeNo == cache.tradeNo) {
-         flag = true
-        }
+    for (let log of logs) {
+      const newLog = CrawlerDBService._diffLogInCache(log, cachedLogs);
+      if (newLog) newLogs.push(newLog);
     }
-    return flag?undefined:log
+    return newLogs;
   }
 
+  static _diffLogInCache(log, cachedLogs) {
+    let flag = false;
+    for (let cache of cachedLogs) {
+      if (log.tradeNo == cache.tradeNo) {
+        flag = true;
+      }
+    }
+    return flag ? undefined : log;
+  }
 }
 
 module.exports = CrawlerDBService;
