2a682e4d558edb1e55a38de1784070dc459674de
Jeremy Guan <jeremyguan1@gmail.com>
2019-05-29 10:52:55 -0700

    good

--- handsomeJ.js
+++ handsomeJ.js
@@ -10,10 +10,8 @@ class App {
     this.timer = null;
   }
 
-  /**
-   * Start of FormatingList Function
-   */
-  static formatingList(tradeNum, amount) {
+  /** Start of FormatingList Function **/
+  static _formatingList(tradeNum, amount) {
     let tempArray = [];
     for (let i = 0; i < tradeNum.length; i++) {
       tempArray.push({
@@ -23,13 +21,39 @@ class App {
     }
     return tempArray;
   }
-  /**
-   * End of FormatingList Function
-   */
+  /**  End of FormatingList Function **/
 
-  /**
-   * Start of Init Function
-   */
+  /** Start of _saveDataToDataBase Function **/
+  async _saveDataToDataBase(data) {
+    let foundLog = App._formatingList(data.tradeNum, data.amount);
+    let dataBase = new DBServer("asdf");
+    let tempArray = await dataBase.compareLogs(foundLog);
+    if (tempArray.length > 0) {
+      //TODO: send data to king
+      console.log(chalk.red(JSON.stringify(tempArray)));
+      dataBase.saveLogs(tempArray);
+    }
+  }
+  /**  End of _saveDataToDataBase Function **/
+
+  /** Start of _getBrowserData Function **/
+  async _getBrowserData() {
+    let data = await this.page.evaluate(() => {
+      let tradeNum = Array.from(document.querySelectorAll("p"))
+        .map(trade => {
+          return trade.textContent.includes("流水号") ? trade.textContent.split(":")[1] : null;
+        })
+        .filter(el => {
+          return el != null;
+        });
+      const amount = Array.from(document.querySelectorAll("table tr td .amount-pay"));
+      return { tradeNum, amount: amount.map(am => am.textContent) };
+    });
+    return data;
+  }
+  /** End of _getBrowserData Function **/
+
+  /**  Start of Init Function **/
   async init() {
     this.browser = await puppeteer.launch({
       headless: false
@@ -38,13 +62,9 @@ class App {
     this.timer = null;
     //await this.page.emulate(devices["iPad"]);
   }
-  /**
-   * End of Init Function
-   */
+  /**  End of Init Function **/
 
-  /**
-   * Start of Redirect Function
-   */
+  /** Start of Redirect Function **/
   async redirect() {
     let random = Math.floor(Math.random(5) * 1000);
     try {
@@ -57,26 +77,20 @@ class App {
       console.log(chalk.red("error when redirect"));
     }
   }
-  /**
-   * End of Redirect Function
-   */
+  /**  End of Redirect Function **/
 
-  /**
-   * Start of StopApp Function
-   */
-  stopApp() {
-    this.page = null;
-    clearTimeout(this.timer);
-    this.browser.close();
+  /** Start of StopApp Function **/
+  stop() {
+    if (this.page) {
+      this.page = null;
+      clearTimeout(this.timer);
+      this.browser.close();
+    }
     return;
   }
-  /**
-   * End of StopApp Function
-   */
+  /**  End of StopApp Function **/
 
-  /**
-   * Start of Start Function
-   */
+  /**  Start of Start Function **/
   async start() {
     if (!this.page) {
       await this.init();
@@ -87,28 +101,9 @@ class App {
       // await this.page.waitForNavigation();
       await this.page.waitFor("#tradeRecordsIndex");
       console.log(chalk.blue("========== 寻找数据成功 ========"));
-      const data = await this.page.evaluate(() => {
-        let tradeNum = Array.from(document.querySelectorAll("p"))
-          .map(trade => {
-            return trade.textContent.includes("流水号") ? trade.textContent.split(":")[1] : null;
-          })
-          .filter(el => {
-            return el != null;
-          });
-        const amount = Array.from(document.querySelectorAll("table tr td .amount-pay"));
-        return { tradeNum, amount: amount.map(am => am.textContent) };
-      });
-
+      const data = await this._getBrowserData();
       if (data) {
-        console.log(App.formatingList(data.tradeNum, data.amount));
-        let foundLog = App.formatingList(data.tradeNum, data.amount);
-        let dataBase = new DBServer("asdf");
-        let tempArray = await dataBase.compareLogs(foundLog);
-        if (tempArray.length > 0) {
-          //TODO: send data to king
-          console.log(chalk.red(JSON.stringify(tempArray)));
-          dataBase.saveLogs(tempArray);
-        }
+        this._saveDataToDataBase(data);
         this.redirect();
       }
     } catch (error) {
@@ -122,20 +117,14 @@ class App {
       console.log(chalk.red(error.message));
     }
   }
-  /**
-   * End of Start Function
-   */
+  /**  End of Start Function **/
 }
 
-/**
- * Start of Run Function
- */
+/**  Start of Run Function **/
 function run() {
   let newApp = new App();
   newApp.start();
 }
-/**
- * End of Run Function
- */
+/**  End of Run Function **/
 
 run();
